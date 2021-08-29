80ce7fac8852e246927e629c1f0ead0ecb7f865a
unknown <mindquaker20@gmail.com>
2021-08-28 22:01:11 -0400

    Updated all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+//eslint-disable
+import "./App.css";
+import React from "react";
+import visa from "./images/visa.png";
+import taxes from "./images/taxes.png";
+import legalDoc from "./images/legalDoc.jpg";
+import notery from "./images/notery.png";
+import passport_ticket from "./images/passport_ticket.jpg";
+import travel from "./images/travel.png";
+import photography from "./images/photography.png";
+import ticket from "./images/ticket.png";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-full w-full  '>
+      <div
+        id='home'
+        className='h-3/2 w-5/6 m-auto mt-10 flex flex-row rounded-3xl bg-blue-500 shadow-lg 
+        
+        '>
+        <div className='flex flex-col h-full relative'>
+          <img src={visa} alt='visa img' className='h-40 absolute w-40 ml-72' />
+          <img
+            src={taxes}
+            alt='taxes ima'
+            className='h-32 w-28 absolute ml-20 mt-36'
+          />
+          <img
+            src={legalDoc}
+            alt='legalDocs img'
+            className='h-28 w-24 ml-64 absolute mt-56 '
+          />
+          <img src={notery} alt='notery img' className='h-32 w-28 ml-5' />
+        </div>
+        <div className='m-auto'>
+          <div className='h-56 m-auto mb-2 w-56  mt-10 ring-4 ring-white rounded-full bg-gray-100'></div>
+          <h1
+            className='text-center text-7xl mb-4 font-bold text-black
+            
+            '>
+            Badal Rana
+          </h1>
+          <h1 className='text-center text-3xl  mb-2 text-extrabold text-black'>
+            Ph: +1 862-571-1255
+          </h1>
+        </div>
+        <div className='flex flex-col relative'>
+          <img
+            src={passport_ticket}
+            alt='passport img'
+            className='h-32 w-28 -ml-56 mt-2 absolute'
+          />
+          <img
+            src={ticket}
+            alt='ticket img'
+            className='h-32 w-28  absolute mt-60 -ml-20'
+          />
+          <img
+            src={travel}
+            alt='travel-img'
+            className='h-32 w-28 absolute mt-36 -ml-80'
+          />
+          <img
+            src={photography}
+            alt='photography img'
+            className='h-32 w-28 mr-3 mt-8'
+          />
+        </div>
+      </div>
+
+      <Nav />
+
+      <Home />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,72 @@
+//eslnt-disable
+import React from "react";
+export const Nav = () => {
+  const style =
+    "p-3 text-black-200 hover:text-indigo-400  cursor-pointer hover:shadow-md transition ease-in duration-150 ";
+  return (
+    <div className='bg-blue-500   w-1/2 m-auto mt-2   rounded-xl shadow-lg '>
+      <navbar>
+        <nav className='h-12  w-full space-x-7 flex flex-row items-center justify-left'>
+          <a
+            href='#'
+            className='p-3 text-black-200 hover:text-indigo-400  cursor-pointer hover:shadow-md transition ease-in duration-150'>
+            Home
+          </a>
+          <div className='relative group'>
+            <button class='flex flex-row items-center w-full   mt-2   text-left  bg-transparent rounded-lg md:w-auto md:inline md:mt-0 md:ml-4 focus:outline-none font-montserrat'>
+              <span className={style}>Services</span>
+            </button>
+            <div class='absolute z-10 hidden bg-grey-200 group-hover:block'>
+              <div class='px-2 pt-2 pb-4 bg-gray-200 shadow-lg'>
+                <div class=''>
+                  <a href='#service1' className={style}>
+                    Service 1{" "}
+                  </a>
+                  <br />
+                  <a href='#service2' className={style}>
+                    Service 2{" "}
+                  </a>
+                  <br />
+
+                  <a href='#service3' className={style}>
+                    Service 3{" "}
+                  </a>
+
+                  <br />
+                  <a href='#service4' className={style}>
+                    Service 4{" "}
+                  </a>
+
+                  <br />
+
+                  <a href='#service5' className={style}>
+                    Service 5{" "}
+                  </a>
+
+                  <br />
+
+                  <a href='#service6' className={style}>
+                    Service 6{" "}
+                  </a>
+                  <br />
+                  <a href='#service7' className={style}>
+                    Service 7{" "}
+                  </a>
+
+                  <br />
+
+                  <a href='#service8' className={style}>
+                    Service 8{" "}
+                  </a>
+                  <br />
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </navbar>
+    </div>
+  );
+};
+
+export default Nav;
